e734f7dc9d72ec1bb7b6209fe77746e694f73ba3
thaunghtikelin12 <klin0330@gamil.com>
2023-12-27 03:18:54 +0630

    First Commit

---
+++ note.py
@@ -0,0 +1,38 @@
+### Data Type
+## String 
+#" " has method,slicing,format,
+# print("hello world")
+# print("The resutl of {0} and {1} is \n{2}".format(1,2,1+2))
+
+# import math
+# print(f'The value of pi is approximately {math.pi:.3f}.')
+
+# bugs = 'roaches'
+# count = 13
+# area = 'living room'
+# print(f'Debugging {bugs=} {count=} {area=}')
+#_________________________________________________________________
+##Number 
+#int,float,complex
+#_________________________________________________________________
+##Boolean
+#True,#False
+#_________________________________________________________________
+##Array
+#Methods,Slicing(start:end:step),index,changeable
+# lis1=['abb',2,1.23,1+5j,True,False] *****Ordered,Changeable,Allow Duplicates,The list() Constructor
+# print(lis1[::-1])
+
+Tup1=('aa','bb','cc')  #       *****Ordered,Unchangeable,Allow Duplicates,The tuple() Constructor
+# print(Tup1.index('aa'))
+(AA,BB,CC)=Tup1
+print(AA)
+
+# Dic1={'abc':1,1:'abc',True:"haha","mylist":lis1[::-1]}
+# print(Dic1["mylist"])
+
+# set1={1,2,3,4,5}
+# set2={3,4,5,6,7,8}
+# print(set1-set2)#| , & , set1 - set2 , set2 - set1 , set1^set2
+
+
